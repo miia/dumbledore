63cec3a8e1ed0d6f19cbb5ad17e2352655160a65
Simone Baratta <Conte91@gmail.com>
2014-08-04 16:20:27 +0200

    Enable data forwarding into datapath

--- vhdlsim/DLX_datapath.vhd
+++ vhdlsim/DLX_datapath.vhd
@@ -21,6 +21,8 @@ ENTITY DLX_DATAPATH IS
  -- EX Control Signals
  S1           : in std_logic;  -- MUX-A Sel
  S2           : in std_logic;  -- MUX-B Sel
+ SELECT_REGA : in std_logic_vector(1 downto 0);
+ SELECT_REGB : in std_logic_vector(1 downto 0);
  ALU         : in ALUOP; -- ALU Operation Code
  EN2      : in std_logic;  -- ALU Output Register Enable
  ALU_FLAGS_OUT: out ALU_FLAGS;
@@ -47,6 +49,8 @@ ARCHITECTURE dlx_simple OF DLX_DATAPATH IS
   signal pipe1in2_in, pipe1in2_out, memory_out, pipe3out_out: REGISTER_CONTENT;-- We put immediate in the B side of the ALU so that we can make SUBI
   signal pipe1rd1_out, pipe2rd2_out: REG_ADDRESS;
   signal ALU_A, ALU_B, ALU_OUT, pipe2aluout_out, pipe2me_out, writeback_data: REGISTER_CONTENT;-- Data input for ALU
+  signal regO_out, regBa_out, rightA_out, rightB_out: REGISTER_CONTENT;-- Data input for ALU
+  signal input_for_right_a, input_for_right_b: mux_generic_input(3 downto 0, REGISTER_SIZE-1 downto 0);
   
 BEGIN
     
@@ -84,16 +88,52 @@ BEGIN
 
 
   --EXECUTE STAGE
+
+  ----------------------------------------------------------------------------------------------------------------------------
+  --Select the right value for read registers and overwrite register which still have not been writeback'd (data forwarding)
+
+  --This should be a std_logic_vector assignment, no comment
+  --Selection is driven from outside: 0 is "0000", 1 is real register, 2 is input from regO, 3 is regBa
+  assign_values: for i in 0 to REGISTER_SIZE-1 loop
+
+    input_for_right_a(0,i)<='0';
+    input_for_right_a(1,i)<=pipe1a_out(i);
+    input_for_right_a(2,i)<=regO_out(i);
+    input_for_right_a(3,i)<=regBa_out(i);
+
+    input_for_right_b(0,i)<='0';
+    input_for_right_b(1,i)<=pipe1b_out(i);
+    input_for_right_b(2,i)<=regO_out(i);
+    input_for_right_b(3,i)<=regBa_out(i);
+  end loop;
+
+  which_is_right_a: ENTITY work.MUX_GENERIC
+  GENERIC MAP(WIDTH => REGISTER_SIZE, HEIGHT => 4) PORT MAP(A => input_for_right_a, S => SELECT_REGA, Y => rightA_out);
+
+  which_is_right_b: ENTITY work.MUX_GENERIC
+  GENERIC MAP(WIDTH => REGISTER_SIZE, HEIGHT => 4) PORT MAP(A => input_for_right_b, S => SELECT_REGB, Y => rightB_out);
+  ----------------------------------------------------------------------------------------------------------------------------
+
+
   select_immediate: ENTITY work.MUX21_GENERIC
-  GENERIC MAP(WIDTH => REGISTER_SIZE) PORT MAP(A=>pipe1B_out, B=> pipe1in2_out, S=>S1, Y => ALU_B);
+  GENERIC MAP(WIDTH => REGISTER_SIZE) PORT MAP(A=>rightB_out, B=> pipe1in2_out, S=>S1, Y => ALU_B);
 
   myAlu: ENTITY work.DLX_ALU
-  PORT MAP(A => pipe1A_out, B=> ALU_B, OP => ALU, Y => ALU_OUT, FLAGS => ALU_FLAGS_OUT, Y_extended => OPEN); 
+  PORT MAP(A => rightA_out, B=> ALU_B, OP => ALU, Y => ALU_OUT, FLAGS => ALU_FLAGS_OUT, Y_extended => OPEN); 
 
   --EXEC PIPES
   --Note/TODO: it should be smart to attach alu B operand to memory data input so that we can store immediates
+
   pipe2aluout: ENTITY work.REG_GENERIC
   GENERIC MAP(WIDTH => REGISTER_SIZE) PORT MAP(D => ALU_OUT, CK => CLK, RESET => RESET, Q => pipe2aluout_out);
+
+  --This registers store ALU OUT from the previous operations, and are used for data forwarding through the pipeline.
+  -- Note that pipe2aluout is doubled here to regO; this is because pipe2aluout will be changed to a LATCH instead of a register, but a latch CAN'T be used for this purpose as it could introduce an infinite loop if regO is read (and written) from the ALU in transparent mode.
+  regO: ENTITY work.REG_GENERIC
+  GENERIC MAP(WIDTH => REGISTER_SIZE) PORT MAP(D => ALU_OUT, CK => CLK, RESET => RESET, Q => regO_out);
+
+  regBa: ENTITY work.REG_GENERIC
+  GENERIC MAP(WIDTH => REGISTER_SIZE) PORT MAP(D => writeback_data, CK => CLK, RESET => RESET, Q => regBa_out);
 
   pipe2me: ENTITY work.REG_GENERIC
   GENERIC MAP(WIDTH => REGISTER_SIZE) PORT MAP(D => pipe1b_out, CK => CLK, RESET => RESET, Q => pipe2me_out);
@@ -111,7 +151,7 @@ BEGIN
   --MEM/WB PIPES - What is "OUT" needed to?
   --MEM/WB STAGE
   memory: ENTITY work.DATA_MEMORY
-  PORT MAP(CLK => CLK, RESET => RESET, RD1 => RM, WR => WM, ENABLE => EN3, ADD_RD1 => pipe2aluout_out, ADD_WR => pipe2aluout_out, DATAIN => pipe2me_out, OUT1 => memory_out);
+  GENERIC MAP(NREGS => 1024, REG_WIDTH => 32) PORT MAP(CLK => CLK, RESET => RESET, RD1 => RM, WR => WM, ENABLE => EN3, ADD_RD1 => pipe2aluout_out, ADD_WR => pipe2aluout_out, DATAIN => pipe2me_out, OUT1 => memory_out);
 
   pipe3out: ENTITY work.REG_GENERIC
   GENERIC MAP(WIDTH => REGISTER_SIZE) PORT MAP(D => writeback_data, CK => CLK, RESET => RESET, Q => pipe3out_out);
