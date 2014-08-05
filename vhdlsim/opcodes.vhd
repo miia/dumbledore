7b7061ffee6cebc530ecd37e837d1f4425ebd33e
Simone Baratta <Conte91@gmail.com>
2014-08-05 10:49:51 +0200

    Add OPCODE_J

--- vhdlsim/opcodes.vhd
+++ vhdlsim/opcodes.vhd
@@ -8,11 +8,12 @@ package opcodes is
   constant OPCODE_SUB: CODE := "000000";
   constant OPCODE_ADDI: CODE := "000000";
   constant OPCODE_SUBI: CODE := "000000";
+  constant OPCODE_J: CODE := "000010";
+  constant OPCODE_NOP: CODE := "010101";
   constant OPCODE_LB: CODE := "100000";
   constant OPCODE_LH: CODE := "100001";
   constant OPCODE_LW: CODE := "100011";
   constant OPCODE_LBU: CODE := "100100";
   constant OPCODE_LHU: CODE := "100101";
-  constant OPCODE_NOP: CODE := "010101";
 end package;
 
