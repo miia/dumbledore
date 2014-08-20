311bbc085de942f4679c0ed583ce02e4721c110f
Simone Baratta <Conte91@gmail.com>
2014-08-21 00:40:47 +0200

    Fix TB_DLX_ALU for r types

--- vhdlsim/ALU/TB-DLX_alu.vhd
+++ vhdlsim/ALU/TB-DLX_alu.vhd
@@ -2,6 +2,7 @@ LIBRARY IEEE;
 use IEEE.std_logic_1164.ALL;
 use IEEE.numeric_std.ALL;
 use work.myTypes.ALL;
+use work.txt_util.ALL;
 
 ENTITY TB_DLX_ALU is
 END TB_DLX_ALU;
@@ -17,9 +18,20 @@ ARCHITECTURE boh OF TB_DLX_ALU IS
 
   signal Cout: std_logic_vector(1 downto 0);
   signal OP: ALUOP := "000000000";
+ 
+  function msgS(A: std_logic_vector;  B:std_logic_vector;  Y : std_logic_vector; op: string) return string is
+  begin
+    return integer'image(to_integer(signed(A)) ) & op & integer'image(to_integer(signed(B))) & " = " & integer'image(to_integer(signed(Y)));
+  end function;
+  function msgU(A: std_logic_vector;  B:std_logic_vector;  Y : std_logic_vector; op: string) return string is
+  begin
+    return integer'image(to_integer(unsigned(A)) ) & op & integer'image(to_integer(unsigned(B))) & " = " & integer'image(to_integer(unsigned(Y)));
+  end function;
+  function msgV(A: std_logic_vector;  B:std_logic_vector;  Y : std_logic_vector; op: string) return string is
+  begin
+    return str(A) & op & str(B) & " = " & str(Y);
+  end function;
 begin
-
-  
   change_op: process
     variable ru: unsigned(REGISTER_SIZE-1 downto 0);
     variable rul: unsigned(REGISTER_SIZE*2-1 downto 0);
@@ -28,107 +40,103 @@ begin
     variable temp: std_logic;
     variable rand_temp: REGISTER_CONTENT;
   begin
-      B <= A;
-      rand_temp := A;
-      temp := rand_temp(REGISTER_SIZE-1) xor rand_temp(REGISTER_SIZE-2);
-rand_temp(REGISTER_SIZE-1 downto 1) := rand_temp(REGISTER_SIZE-2 downto 0);
-rand_temp(0) := temp;
-   A <= rand_temp;
+    --Generate new values for the operands
+    B <= A;
+    rand_temp := A;
+    temp := rand_temp(REGISTER_SIZE-1) xor rand_temp(REGISTER_SIZE-2);
+    rand_temp(REGISTER_SIZE-1 downto 1) := rand_temp(REGISTER_SIZE-2 downto 0);
+    rand_temp(0) := temp;
+    A <= rand_temp;
  
-    OP <= "000000000";
+    OP(4 downto 3) <= "00"; -- Arithmetic unit
+    
     --ADD/ADDU
+    OP(2 downto 0) <= "000";
     wait for 10 ns;
     Ru := Au+Bu;
     ASSERT(Yu=Ru);
     
     --SUB/SUBU
-    OP <= std_logic_vector(unsigned(OP)+1);
+    OP(2 downto 0) <= "001";
     wait for 10 ns;
     Rs:=As-Bs;
-    ASSERT(signed(Y)=Rs) REPORT integer'image(to_integer(signed(A)) ) & "+" & integer'image(to_integer(signed(B))) & " = " & integer'image(to_integer(signed(Y))) SEVERITY FAILURE;
+    ASSERT(signed(Y)=Rs) REPORT msgS(A, B, Y, "-") SEVERITY FAILURE;
 
     --MUL
-    OP <= std_logic_vector(unsigned(OP)+1);
+    OP(2 downto 0) <= "010";
     wait for 10 ns;
     Rsl:=(As*Bs);
-    ASSERT (Y2s)= Rsl REPORT integer'image(to_integer(Au))  & "*" &  integer'image(to_integer(Bu)) &  " = " & integer'image(to_integer(Y2u)) SEVERITY FAILURE;
+    ASSERT (Y2s)= Rsl REPORT msgS(A,B,Y2 & Y,"*") SEVERITY FAILURE;
     
     --MUL(to be changed to IMUL)
-    OP <= std_logic_vector(unsigned(OP)+1);
+    OP(2 downto 0) <= "011";
     wait for 10 ns;
     Rsl:=(As*Bs);
-    ASSERT (Y2s)= Rsl REPORT integer'image(to_integer(Au))  & "*" &  integer'image(to_integer(Bu)) &  " = " & integer'image(to_integer(Y2u)) SEVERITY FAILURE;
+    ASSERT (Y2s)= Rsl REPORT msgS(A,B,Y2 & Y,"*") SEVERITY FAILURE;
 
+   OP(4 downto 3) <= "01"; -- Logic unit
    --OR
-   OP <= std_logic_vector(unsigned(OP)+1);
+   OP(2 downto 0) <= "000";
    wait for 10 ns;
-   ASSERT(Y=(A or B)) SEVERITY FAILURE;
+   ASSERT(Y=(A or B)) REPORT msgV(A,B, Y, "|") SEVERITY FAILURE;
     
    --and
-   OP <= std_logic_vector(unsigned(OP)+1);
+   OP(2 downto 0) <= "001";
    wait for 10 ns;
-   ASSERT(Y=(A and B)) SEVERITY FAILURE;
+   ASSERT(Y=(A and B)) REPORT msgV(A,B,Y,"&") SEVERITY FAILURE;
      
    --xOR
-   OP <= std_logic_vector(unsigned(OP)+1);
+   OP(2 downto 0) <= "010";
    wait for 10 ns;
-   ASSERT(Y=(A xor B)) SEVERITY FAILURE;
+   ASSERT(Y=(A xor B)) REPORT msgV(A,B,Y,"^") SEVERITY FAILURE;
 
        
    --NOT
-   OP <= std_logic_vector(unsigned(OP)+1);
+   OP(2 downto 0) <= "011";
    wait for 10 ns;
-   ASSERT(Y=not A) SEVERITY FAILURE;
+   ASSERT(Y=not A) REPORT msgV(A,B,Y,"!") SEVERITY FAILURE;
+
+   OP(4 downto 3) <= "10";
+
+   --Logic shift left
+   OP(2 downto 0) <= "111";
+   wait for 10 ns;
+   ASSERT(Ys=(As sll to_integer(unsigned(B(4 downto 0))))) REPORT msgV(A, B, Y, "sll") SEVERITY FAILURE;
+
+   --Logic shift right
+   OP(2 downto 0) <= "110";
+   wait for 10 ns;
+   ASSERT(Ys=(As srl to_integer(unsigned(B(4 downto 0))))) REPORT msgV(A, B, Y, "srl") SEVERITY FAILURE;
    
+   --Logic rotate left
+   OP(2 downto 0) <= "101";
+   wait for 10 ns;
+   ASSERT(Ys=(As rol to_integer(unsigned(B(4 downto 0))))) REPORT msgV(A, B, Y, "rol") SEVERITY FAILURE;
    
+   --Logic rotate right
+   OP(2 downto 0) <= "100";
+   wait for 10 ns;
+   ASSERT(Ys=(As ror to_integer(unsigned(B(4 downto 0))))) REPORT msgV(A, B, Y, "ror") SEVERITY FAILURE;
+      
+   --Arith shift left
+   OP(2 downto 0) <= "011";
+   wait for 10 ns;
+   --ASSERT(Ys=(As sla to_integer(unsigned(B(4 downto 0))))) REPORT msgV(A, B, Y, "sla") SEVERITY FAILURE;
+
+   --Arith shift right
+   OP(2 downto 0) <= "010";
+   wait for 10 ns;
+   --ASSERT(Ys=(As sra to_integer(unsigned(B(4 downto 0))))) REPORT msgV(A, B, Y, "sra") SEVERITY FAILURE;
    
-   ---RESTART WITH IMMEDIATE BIT SET TO 1
+   --Arith rotate left
+   OP(2 downto 0) <= "001";
+   wait for 10 ns;
+   ASSERT(Ys=(As rol to_integer(unsigned(B(4 downto 0))))) REPORT msgV(A, B, Y, "rol") SEVERITY FAILURE;
    
-   OP <= "000010000";    
-      --ADD/ADDU
-      wait for 10 ns;
-      Ru := Au+Bu;
-      ASSERT(Yu=Ru);
-      
-      --SUB/SUBU
-      OP <= std_logic_vector(unsigned(OP)+1);
-      wait for 10 ns;
-      Rs:=As-Bs;
-      ASSERT(signed(Y)=Rs) REPORT integer'image(to_integer(signed(A)) ) & "+" & integer'image(to_integer(signed(B))) & " = " & integer'image(to_integer(signed(Y))) SEVERITY FAILURE;
-  
-      --MUL
-      OP <= std_logic_vector(unsigned(OP)+1);
-      wait for 10 ns;
-      Rsl:=(As*Bs);
-      ASSERT (Y2s)= Rsl REPORT integer'image(to_integer(Au))  & "*" &  integer'image(to_integer(Bu)) &  " = " & integer'image(to_integer(Y2u)) SEVERITY FAILURE;
-      
-      --MUL(to be changed to IMUL)
-      OP <= std_logic_vector(unsigned(OP)+1);
-      wait for 10 ns;
-      Rsl:=(As*Bs);
-      ASSERT (Y2s)= Rsl REPORT integer'image(to_integer(Au))  & "*" &  integer'image(to_integer(Bu)) &  " = " & integer'image(to_integer(Y2u)) SEVERITY FAILURE;
-  
-     --OR
-     OP <= std_logic_vector(unsigned(OP)+1);
-     wait for 10 ns;
-     ASSERT(Y=(A or B)) SEVERITY FAILURE;
-      
-     --and
-     OP <= std_logic_vector(unsigned(OP)+1);
-     wait for 10 ns;
-     ASSERT(Y=(A and B)) SEVERITY FAILURE;
-       
-     --xOR
-     OP <= std_logic_vector(unsigned(OP)+1);
-     wait for 10 ns;
-     ASSERT(Y=(A xor B)) SEVERITY FAILURE;
-  
-     --LHI
-     OP <= std_logic_vector(unsigned(OP)+1);
-     OP(5) <= '1'; --this time we need to pre-select output of LH block (instead of Logic Unit).
-     wait for 10 ns;
-     ASSERT(Y=(B(15 downto 0) & "0000000000000000")) SEVERITY FAILURE;
-      
+   --Arith rotate right
+   OP(2 downto 0) <= "000";
+   wait for 10 ns;
+   ASSERT(Ys=(As ror to_integer(unsigned(B(4 downto 0))))) REPORT msgV(A, B, Y, "ror") SEVERITY FAILURE;
   end process;
   
   testcomp: ENTITY work.DLX_ALU
