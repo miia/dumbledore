19fb1ad649791b191692959ad8a7579e33a7dcc8
Simone Baratta <Conte91@gmail.com>
2014-08-22 20:03:31 +0200

    Time for some tests: add TB_DLX BD

---
+++ vhdlsim/TB_DLX.vhd
@@ -0,0 +1,15 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+
+ENTITY TB_DLX IS
+END TB_DLX;
+ARCHITECTURE TEST OF TB_DLX IS
+  signal CLK: std_logic := '0';
+  signal RESET: std_logic;
+  signal POUT: std_logic_vector(31 downto 0); -- Connected to R30
+BEGIN
+  -- Fetch unit will automatically fetch instruction and execute them, all we have to do is let it go :)
+  toTest: ENTITY work.DLX PORT MAP(CLK, RESET, POUT);
+  RESET <= '0', '1' after 30 ns;
+  CLK <=  not CLK after 10 ns;
+END ARCHITECTURE;
