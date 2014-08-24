5cad8c64357114052c6183e28f4d2a6eb825b074
Simone Baratta <Conte91@gmail.com>
2014-08-24 07:47:17 +0200

    test_math+=1 instruction

--- vhdlsim/software/test_math.asm
+++ vhdlsim/software/test_math.asm
@@ -2,6 +2,7 @@
 ;r4=0;
 ;r1=10;
 ;r2=30;
+;r4=r1+r2;
 ;r30=R4+r1;
 ;r30--;
 ; 
@@ -9,6 +10,7 @@ thastart:
 add r4,r0,r0
 addi r1, r0, 10;
 addi r2, r0, 30;
+add r4, r2, r1;
 add r30, r4, r1;
 subi r30, r30, 1;
 j thastart
