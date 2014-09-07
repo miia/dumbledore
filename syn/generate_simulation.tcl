807017063e9e16d188df79f4843db414da689b09
Simone Baratta <Conte91@gmail.com>
2014-09-07 07:53:28 +0200

    Done post-synthesis simulation, little .ps example written out :)

--- syn/generate_simulation.tcl
+++ syn/generate_simulation.tcl
@@ -1,6 +1,8 @@
 read_file -format ddc results/DLX.ddc
 ungroup -flatten -all
-write_file -format ddc results/SYN_DLX.ddc
-write_file -format vhdl results/SYN_DLX.vhdl
+#This command is necessary to avoid having into sdf file net names like a/b, as they are ungrouped by dc_shell but would be treated as hierarchical by modelsim
+change_names -rules vhdl -hierarchy
+write_file -format ddc -output results/SYN_DLX.ddc
+write_file -format vhdl -output results/SYN_DLX.vhdl
 #"Version 2.1" is reccomended for compatibility purposes
 write_sdf -version 2.1 results/SYN_DLX.sdf
--- vhdlsim/DLX_syn.do
+++ vhdlsim/DLX_syn.do
@@ -1,6 +1,6 @@
 onerror {resume}
 quietly WaveActivateNextPane {} 0
-vsim -sdftyp /totest/=../syn/results/SYN_DLX.sdf work.tb_dlx_postsyn
+vsim -novopt -sdftyp /tb_dlx_postsyn/totest/=../syn/results/SYN_DLX.sdf work.tb_dlx_postsyn -sdfnoerror -t 1fs
 source SYN_DLX_wave.do
 
 update
