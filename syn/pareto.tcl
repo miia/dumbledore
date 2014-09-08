b5ad53179c3254e8b9b0566a7c85f5e0c101cc25
Michele Iacobone <michele.iacobone@gmail.com>
2014-09-08 10:05:04 +0200

    Add advanced options to optimization script

--- syn/pareto.tcl
+++ syn/pareto.tcl
@@ -2,6 +2,10 @@ set LIBRARY WORK
 set NETLIST_DIR results/
 set REPORT_DIR reports/
 
+set ADVANCED_OPTS ""
+set TRYHARD 1	;#if 1, adds "-map_effort high -area_effort high -power_effort high -ungroup-all" to advanced options
+set CG 1	;#if 1, adds clock gating to advanced options
+
 proc do_reports {filename} {
   variable NETLIST_DIR
   variable REPORT_DIR
@@ -20,13 +24,19 @@ proc do_reports {filename} {
 #load result of previous pre-synthesis of DLX design (result was exported in a .ddc file) 
 read_file -format ddc results/DLX.ddc
 
+#add advanced options, if any, according to configuration
+if {$TRYHARD == 1} {append ADVANCED_OPTS "-map_effort high -area_effort high -power_effort high -ungroup-all "}
+if {$CG == 1} {append ADVANCED_OPTS "-gate_clock "}
+puts -nonewline "Using advanced options: "
+if {[string match "" $ADVANCED_OPTS] == 1} { puts "\[none\]" } else {puts $ADVANCED_OPTS}
+
 #actual optimization (loops with several different constraints)
 for {set i 4} {$i <= 10} {incr i} {
   for {set i 20} {$i <= 70} {set i [expr $i+10]} {
     puts "Compiling with target clock period of $i ns and target total power of $j uW..."
     create_clock -period $i [get_ports CLK]	;#set timing constraint on clock period
     set_max_total_power $j mW			;#set maximum total power constraint
-    compile -exact_map				;#perform compilation (optimization) again
+    compile -exact_map $ADVANCED_OPTS		;#perform compilation (optimization) again
     puts "Generating report for $i ns clock and $j uW total power"
     do_reports "DLX_${i}ns_${j}uW"
   }
