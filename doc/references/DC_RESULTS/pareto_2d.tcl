d519d6319134d836982e322b8e7ba649a7622247
Michele Iacobone <michele.iacobone@gmail.com>
2014-09-23 18:30:42 +0200

    PICTURES

---
+++ doc/references/DC_RESULTS/pareto_2d.tcl
@@ -0,0 +1,92 @@
+#Finds Pareto points in terms of clock period VS total power (dynamic+leakage);
+#Area should have more or less the SAME value for all found points, since all available timing slack is used to minimize power as much as possible.
+
+
+set LIBRARY WORK
+set NETLIST_DIR TRY_ME5/results/
+set REPORT_DIR TRY_ME5/reports/
+
+#clock periods used for compilation will range from MIN_PERIOD<=clk<=MAX_PERIOD (expressed in ns)
+set MIN_PERIOD 8
+set MAX_PERIOD 8
+
+set ADVANCED_OPTS ""
+
+set TRYHARD 1	;#if 1, adds "-map_effort high -area_effort high -power_effort high -ungroup-all" to advanced options
+set CG 0	;#if 1, adds clock gating to advanced options
+
+set USE_MAX_CAP 1  ;#if 1, adds max fanout constraint for the high-fanout RESET signal - remember to set the value of MAX_FANOUT below
+set MAX_CAP 10
+
+set USE_WLM 0   ;#if 1, uses a non-default wire load model - remember to set the value of WLM to one of the WLM contained in the current library (which is specified in the .synopsys_dc.setup file).
+set WLM "0K_hvratio_1_4"
+
+proc do_reports {filename} {
+  variable NETLIST_DIR
+  variable REPORT_DIR
+
+  #For each optimized design:
+
+  #1- Save .sdc constraints file (contains information about the defined clock tree and its timing constraints, which will be used by the post-synthesis layout tool)
+  write_sdc $NETLIST_DIR/$filename.sdc
+
+
+  #2- Write post-synthesis HDL files: the VHDL file can be used in a simulation tool (e.g. Modelsim) for post-synthesis simulation with accurate timing information; the Verilog file will be used as starting point (together with the .sdc file) by the post-synthesis layout tool (Encounter).
+  #write_file -format vhdl -hierarchy -output $NETLIST_DIR/$filename.vhd
+  write_file -format verilog -hierarchy -output $NETLIST_DIR/$filename.v
+
+  #3- Write the reports and a .ddc file (makes it possible to easily resume synthesis later).
+  write_file -format ddc -hierarchy -output $NETLIST_DIR/$filename.ddc
+  report_area -nosplit > $REPORT_DIR/${filename}_area.rpt
+  report_timing -nworst 10 > $REPORT_DIR/${filename}_timing.rpt
+  report_power > $REPORT_DIR/${filename}_power.rpt
+  report_net_fanout > $REPORT_DIR/${filename}_fanout.rpt
+}
+
+#add advanced options, if any, according to configuration
+if {$TRYHARD == 1} {append ADVANCED_OPTS "-map_effort high -area_effort high -power_effort high "}
+if {$CG == 1} {append ADVANCED_OPTS "-gate_clock "}
+puts -nonewline "Using advanced options: "
+if {[string match "" $ADVANCED_OPTS] == 1} { puts "\[none\]" } else {puts $ADVANCED_OPTS}
+
+#actual optimization (loops with several different constraints)
+for {set i $MIN_PERIOD} {$i <= $MAX_PERIOD} {incr i} {
+  #load result of previous pre-synthesis of DLX design (result was exported in a .ddc file) 
+  read_file -format ddc results/DLX.ddc
+
+  #if {$i == 4 && $j == 20} {continue} ;#has already been done
+  puts "Compiling with primary constraint of ${i}ns clock period and 2nd constraint of minimum possible power..."
+
+  create_clock -period $i [get_ports CLK]		;#set timing constraint on clock period; note this includes "set_dont_touch" and "set_ideal_net"
+
+  set_max_total_power 0.0 mW				;#find the minimum possible power that still meets the timing constraint; area will follow as third parameter (=> recover area only where it comes absolutely for free)
+
+ if {$USE_WLM == 1} {set_wire_load_model -name $WLM}	;#characterize wires as a non-ideal load;
+
+  #HIGH-FANOUT CLOCK SIGNAL:
+  #clock tree will not have buffers inserted during synthesis; it will be left out of the optimization, so that buffers will not be inserted - they will be inserted with ACCURATE balancing by Encounter, after the routing phase.
+  set_propagated_clock [all_clocks]     ;# specifies that the propagation delay is NOT zero; during synthesis, clock skew along the clock tree will be calculated according to the wire load model used, annotated when exporting the .sdc file, and later used by Encounter for Clock Tree Synthesis.
+
+  #HIGH-FANOUT RESET SIGNAL:
+  #differently from clk, balancing the tree is not fundamental here, so buffer insertion can be made in an approximate way by DC itself, during synthesis (see balance_buffer command later on)
+  if {$USE_MAX_CAP == 1} {set_max_capacitance $MAX_CAP RESET}
+  ##set_dont_touch [get_ports RESET]
+  ##set_ideal_net [get_ports RESET]
+  ###set_input_delay 1.5 -clock CLK [get_ports RESET]
+
+  #perform compilation, leaving CLK and RESET alone (no buffer insertion)
+  set command "compile -exact_map $ADVANCED_OPTS"
+  eval $command 					;#perform optimized compilation again
+
+  #after compilation, perform (approximately) balanced buffer insertion on the RESET tree:
+  ##balance_buffer -net [get_ports RESET]
+
+  puts "Generating report for ${i}ns clock (and minimum possible power)"
+  set filename "DLX_${i}ns_minpower"
+  if {$CG == 1} {append filename "_CG"}
+  if {$USE_WLM == 1} {append filename "_WLM"} ;#a non-default wire load model was used
+  if {$USE_MAX_CAP == 1} {append filename "_MCAP"} ;#a maximum fanout constraint was used
+  do_reports $filename
+}
+
+
---
+++ doc/references/DC_RESULTS/synthesis.tcl
@@ -0,0 +1,127 @@
+suppress_message VHD-4
+set LIBRARY WORK
+set NETLIST_DIR results/
+set REPORT_DIR reports/
+
+proc compileexactmap {entity {params ""} } {
+  variable dict
+  variable LIBRARY
+  if { [lsearch $dict $entity] >= 0 } {
+    #Architecture is defined into the list
+    puts "Index: [lsearch $dict $entity]"
+    puts "List: [lindex [lindex $dict [expr [lsearch $dict $entity] + 1]] 0]"
+    set theArch [lindex [lindex $dict [expr [lsearch $dict $entity] + 1]] 0]
+    echo "Compiling $entity with architecture $theArch..."
+    set arch "-architecture $theArch"
+  } else {
+    echo "Compiling $entity with default architecture..."
+    set arch ""
+  }
+  set command "elaborate $entity $arch -library $LIBRARY $params"  ;# "elaborate" builds an internal graph of the selected circuit (if it is a hierarchical circuit, this automatically builds on other entities that were previously analyzed too), and then maps this graph to general (=technology-independent) logic gates.
+  eval $command
+  compile -exact_map  ;# "compile" finally translated our high-level netlist of general logic cells into a netlist of TECHNOLOGY-DEPENDENT cells from the tech library.
+  #NOTE: in this phase we could also specify optimization constraints, but we won't yet: a .ddc file will be generated after the unconstrained optimization (by calling do_reports), and later on, starting from there, more optimized synthesis will be performed again (see pareto.tcl).
+}
+
+proc do_reports {filename} {
+  variable NETLIST_DIR
+  variable REPORT_DIR
+  #NOT YET - annotated vhdl and verilog files take time to produce, and they're only needed for the design that will be fed into Encounter for physical layout (or back into ModelSim for gate-level simulation).
+  #write_file -format vhdl -hierarchy -output $NETLIST_DIR/$filename.vhd
+  #write_file -format verilog -hierarchy -output $NETLIST_DIR/$filename.v
+  write_file -format ddc -hierarchy -output $NETLIST_DIR/$filename.ddc
+  report_area -nosplit > $REPORT_DIR/${filename}_area.rpt
+  report_timing -nworst 10 > $REPORT_DIR/${filename}_timing.rpt
+  report_power > $REPORT_DIR/${filename}_power.rpt
+}
+
+#1) Get all your non-testbench .vhd files with "find ./vhdl/ -name "*.vhd" -not -name "tb*" | sort" !
+# NOTE: don't include the IRAM - it shouldn't be synthesized.
+set vhdfiles {
+./vhdl/000-globals.vhd
+./vhdl/000-opcodes.vhd
+./vhdl/01-ceillog.vhd
+./vhdl/01-log2.vhd
+./vhdl/01-nd2.vhd
+./vhdl/01-txt_util.vhd
+./vhdl/02-fa.vhd
+./vhdl/02-imm_extender.vhd
+./vhdl/02-iv.vhd
+./vhdl/02-latch.vhd
+./vhdl/02-mux21_generic.vhd
+./vhdl/02-mux21.vhd
+./vhdl/02-mux_generic.vhd
+./vhdl/02-reg_generic_enabled.vhd
+./vhdl/02-reg_generic.vhd
+./vhdl/03-delay_block.vhd
+./vhdl/03-rca.vhd
+./vhdl/04-p4_adder.core/04.1-ha.vhd
+./vhdl/04-p4_adder.core/04.2-pg.vhd
+./vhdl/04-p4_adder.core/04.3-pg_network.vhd
+./vhdl/04-p4_adder.core/04.4-generator.vhd
+./vhdl/04-p4_adder.core/04.5-carryselect_block.vhd
+./vhdl/04-p4_adder.core/04.6-carryselect_row.vhd
+./vhdl/04-p4_adder.core/04.7-carry_generator.vhd
+./vhdl/04-p4_adder.vhd
+./vhdl/05-ACC.vhd
+./vhdl/a.a-CU_HW.core/forwarder.vhd
+./vhdl/a.a-CU_HW.vhd
+./vhdl/a.b-Datapath.core/a.b.a-fetch_stage.core/a.b.a.a-PC_ACC.vhd
+./vhdl/a.b-Datapath.core/a.b.a-fetch_stage.core/a.b.a.b-bpu_entity.vhd
+./vhdl/a.b-Datapath.core/a.b.a-fetch_stage.core/a.b.a.c-bpu_v1_not_taken.vhd
+./vhdl/a.b-Datapath.core/a.b.a-fetch_stage.core/a.b.a.c-bpu_v2_taken.vhd
+./vhdl/a.b-Datapath.core/a.b.a-fetch_stage.core/a.b.a.c-bpu_v3_BHT.core/a.b.a.c.a-bht_onerow.vhd
+./vhdl/a.b-Datapath.core/a.b.a-fetch_stage.core/a.b.a.c-bpu_v3_BHT.core/a.b.a.c.b-demux_generic.vhd
+./vhdl/a.b-Datapath.core/a.b.a-fetch_stage.core/a.b.a.c-bpu_v3_BHT.vhd
+./vhdl/a.b-Datapath.core/a.b.a-fetch_stage.vhd
+./vhdl/a.b-Datapath.core/a.b.b-registerfile.vhd
+./vhdl/a.b-Datapath.core/a.b.c-ALU.core/a.b.c.a-arithmetic_unit.core/a.b.c.a.b-boothmul.core/a.b.c.a.b.a-booth_encoder.vhd
+./vhdl/a.b-Datapath.core/a.b.c-ALU.core/a.b.c.a-arithmetic_unit.core/a.b.c.a.b-boothmul.vhd
+./vhdl/a.b-Datapath.core/a.b.c-ALU.core/a.b.c.a-arithmetic_unit.vhd
+./vhdl/a.b-Datapath.core/a.b.c-ALU.core/a.b.c.b-logic_unit.vhd
+./vhdl/a.b-Datapath.core/a.b.c-ALU.core/a.b.c.c-shifter_generic.vhd
+./vhdl/a.b-Datapath.core/a.b.c-ALU.core/a.b.c.d-comparator_generic.vhd
+./vhdl/a.b-Datapath.core/a.b.c-ALU.core/a.b.c.e-loadhigh.vhd
+./vhdl/a.b-Datapath.core/a.b.c-ALU.vhd
+./vhdl/a.b-Datapath.core/a.b.d-memory_stage.vhd
+./vhdl/a.b-DLX_datapath.vhd
+./vhdl/a-DLX.vhd
+}
+
+analyze -library WORK -format vhdl $vhdfiles  ;#check whether each entity is synthesizable or not.
+
+#2) ...and compile/maintain the following by hand -.- (there's no automatic way to choose the right architecture in files that have several, unless we establish a convention about the order in which architectures are specified - e.g. first architecture in the source file gets used).
+#Dictionary containing (entity, architecture) pairs; note there's only one chosen architecture to be synthesized for each of the entities.
+#Entity is key, architecture is value (represented as a list with only one element, for flexibility).
+#More fields can be added here as needed (e.g. custom constraints for each component) by adding elements to the list.
+
+set dict {
+
+    "FA" { "BEHAVIORAL" }
+    "IMM_EXTENDER" { "DATAFLOW" }
+    "IV" { "BEHAVIORAL" }
+    "LATCH_GENERIC" { "behavioural" }
+    "MUX21_GENERIC" { "STRUCTURAL" }
+    "MUX21" { "STRUCTURAL" }
+    "MUX_GENERIC" { "BEHAVIORAL" }
+    "ND2" { "ARCH1" }
+    "REG_GENERIC" { "PIPPO" }
+    "DELAY_BLOCK" { "structural" }
+    "REGISTER_FILE" { "A" }
+    "RCA" { "STRUCTURAL" }
+    "ACC" { "Structural" }
+    "DLX" { "structural" }
+
+}
+
+#actual synthesis step
+foreach {entity argslist} $dict {
+  compileexactmap $entity
+  do_reports $entity    ;#this is also re-done in another script, for each compilation that's re-done using timing constraints - see pareto.tcl
+#  set getout [gets stdin]
+#  if {[string match "q" $getout] == 1} {return}
+}
+
+
+#write_file -format vhdl -hierarchy -output ./synthesized/CU_HW_netlist.vhd
+#report_timing -nworst 10 -max_paths 10 > ./synthesized/CU_HW_timing.rpt
